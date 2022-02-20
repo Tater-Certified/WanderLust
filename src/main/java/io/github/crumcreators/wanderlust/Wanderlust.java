98ef2052ac0845f5e0e0313ce3139c4d5ebaaf33
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2022-02-20 15:09:30 -0500

    Complete restart
    I am restarting this as my envision has changed

--- src/main/java/io/github/crumcreators/wanderlust/Wanderlust.java
+++ src/main/java/io/github/crumcreators/wanderlust/Wanderlust.java
@@ -1,20 +1,10 @@
 package io.github.crumcreators.wanderlust;
 
-import io.github.crumcreators.wanderlust.blocks.ModOres;
-import io.github.crumcreators.wanderlust.items.ModMagic;
-import io.github.crumcreators.wanderlust.items.ModRanged;
-import io.github.crumcreators.wanderlust.items.ModWeapons;
 import net.fabricmc.api.ModInitializer;
-import io.github.crumcreators.wanderlust.blocks.ModBlocks;
 
 public class Wanderlust implements ModInitializer {
     public static String MOD_ID = "wanderlust";
     @Override
     public void onInitialize() {
-        ModBlocks.initializeBlocks();
-        ModWeapons.initializeWeapons();
-        ModMagic.initializeMagic();
-        ModOres.initializeOres();
-        ModRanged.initializeRanged();
     }
 }
